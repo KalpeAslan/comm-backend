3039a3065dfd3fb5aaeb1ddee070fc5787b7e29e
KalpeAslan <79480776+KalpeAslan@users.noreply.github.com>
2024-02-20 17:52:24 +0600

    Delete trash

--- conf.ts
+++
@@ -1,6 +0,0 @@
-export const conf = {
-  userGmail: 'jasik.asik14@gmail.com',
-  passwordGmail: 'Tobakabulov2001',
-  sendAsGmail: 'Aslan kalpe <jasik.asik14@gmail.com>',
-  fileDest: '../files/img/'
-}
